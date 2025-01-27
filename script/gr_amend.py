293e05ab418c084e1ed22ebc40abe10ce55fe8f8
William Navarre <git@navarre.xyz>
2025-01-27 09:45:49 -0500

    Continue work.

--- script/gr_amend.py
+++ script/gr_amend.py
@@ -11,9 +11,9 @@ parcels = geopandas.read_parquet("../medford_shp/medford.pqt")
 parcels = parcels[parcels.SHAPE_Area < 21000]
 parcels = parcels.reset_index()
 
-def go_generic(parcels, make_categories, name):
+def go_generic(parcels, make_categories, name, *, ax_function=None):
     fig, ax = plt.subplots(figsize=(10, 10), dpi=300)
-
+    if ax_function is not None: ax = ax_function(ax)
     draw_categories(
         parcels.to_crs(epsg=3857),
         make_categories(parcels),
@@ -34,6 +34,7 @@ def go_generic(parcels, make_categories, name):
     plt.axis("off")
     cx.add_basemap(ax)
     fig.savefig(name, dpi=216, bbox_inches="tight")
+    return ax
 
 def make_categories_4(parcels):
     return [
@@ -46,6 +47,18 @@ def make_categories_4(parcels):
 def make_categories_sf(parcels):
     return [ Category("SFH", BLACK) ]
 
+def go_lteq_units(parcels, count, **argkv):
+    return go_generic(parcels[parcels.MIN_COUNT <= (count + .1)],
+                      lambda x: [ Category("1 <= UNIT COUNT <= {}".format(count), BLACK) ],
+                      "UNIT_LT_{}.png".format(count),
+                      **argkv)
+
+def go_geq_units(parcels, count, **argkv):
+    return go_generic(parcels[parcels.MIN_COUNT >= (count - .1)],
+                      lambda x: [ Category("UNIT COUNT >= {}".format(count), BLACK) ],
+                      "UNIT_GT_{}.png".format(count),
+                      **argkv)
+    
 def filter_sf(parcels, region=None):
     parcels = parcels[ parcels.MIN_COUNT == 1 ]
     if region is not None:
@@ -54,8 +67,24 @@ def filter_sf(parcels, region=None):
         return parcels
 
 def go(parcels, name): go_generic(parcels, make_categories_4, name)
-    
+
+def make_ax_fixer(xlim, ylim):
+    def ax_fixer(ax):
+        ax.set_xlim(xlim)
+        ax.set_ylim(ylim)
+        return ax
+    return ax_fixer
+
 if __name__ == "__main__":
+    housing_parcels = parcels[parcels.MIN_COUNT > .9]
+    ax_fixer = None
+    for count in [1, 2, 4, 8]:
+        if ax_fixer is None:
+            ax = go_geq_units(housing_parcels, count)
+            ax_fixer = make_ax_fixer(ax.get_xlim(), ax.get_ylim())
+        else:
+            go_geq_units(housing_parcels, count, ax_function=ax_fixer)
+        go_lteq_units(housing_parcels, count, ax_function=ax_fixer)
     go_generic(filter_sf(parcels, regions.GR.SOUTH.rect()),
                make_categories_sf,
                "single_family_homes.png")
